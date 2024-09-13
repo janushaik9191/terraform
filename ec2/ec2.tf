63357481bace3a04ceee46dd66e807ffa633bdc0
meerjan <janushaik9191@gmail.com>
2024-09-13 21:25:06 +0530

    ec2 instance creation by terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "terraform" {
-    ami = data.aws_ami.joindevops.id
+    ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
         Name = "terraform"
-    }
+           }
 }

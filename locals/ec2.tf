a50220905e80072ca446e86e10c8f7c6400a2c1c
meerjan <janushaik9191@gmail.com>
2024-09-16 17:24:00 +0530

    variables by using locals

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "locals" {
   count                  = length(var.instance_names)
   ami                    = data.aws_ami.joindevops.id
-  instance_type          = local.instance_type
+  instance_type          = var.instance_names[count.index] == "mysql" ? "t3.small" : "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh_locals.id]
 
   tags = {
